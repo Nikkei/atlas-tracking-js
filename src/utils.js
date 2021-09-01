c937aa428dfc7cef33c30e87413213fb48c500ce
Hajime Sano <m@hsano.jp>
2021-09-01 15:28:40 +0900

    fix

--- src/utils.js
+++ src/utils.js
@@ -103,10 +103,10 @@ export default class Utils {
         const cookies = this.targetWindow.document.cookie.split(';');
         for(let i = 0; i < cookies.length; i++) {
             let cookie = cookies[i];
-            while (cookie.charAt(0) == ' ') {
+            while (cookie.charAt(0) === ' ') {
                 cookie = cookie.substring(1, cookie.length);
             }
-            if (cookie.indexOf(`${k}=`) == 0) {
+            if (cookie.indexOf(`${k}=`) === 0) {
                 return cookie.substring(`${k}=`.length, cookie.length);
             }else{
                 return '';
