e67b8042d21a804ec837e9b78ce1aadf9c8a1093
Hajime Sano <m@hsano.jp>
2021-09-01 15:11:28 +0900

    fixed a function for reading Cookie

--- src/utils.js
+++ src/utils.js
@@ -100,8 +100,18 @@ export default class Utils {
     }
 
     getC(k) {
-        const cookies = this.targetWindow.document.cookie || '';
-        return ((`; ${cookies};`).match(`; ${k}=([^¥S;]*)`) || [])[1] || '';
+        const cookies = this.targetWindow.document.cookie.split(';');
+        for(let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i];
+            while (cookie.charAt(0) == ' ') {
+                cookie = cookie.substring(1, cookie.length);
+            }
+            if (cookie.indexOf(`${k}=`) == 0) {
+                return cookie.substring(`${k}=`.length, cookie.length);
+            }else{
+                return '';
+            }
+        }
     }
 
     getQ(k) {
