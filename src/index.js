d008f9b5214cd4602d12f418a92a003e3da19c22
Mimori <mimorimori256@gmail.com>
2026-01-22 12:38:50 +0900

    fix: remove `message` event listener in `disableTracking`

--- src/index.js
+++ src/index.js
@@ -352,6 +352,9 @@ export default class AtlasTracking {
         if (options.trackPerformance && options.trackPerformance.enable) {
             targetWindow.removeEventListener('DOMContentLoaded', atlasDOMContentLoadedHandler);
         }
+        if (options.trackThroughMessage && options.trackThroughMessage.enable) {
+            targetWindow.removeEventListener('message', atlasMessageHandler);
+        }
 
         options = {};
     }
